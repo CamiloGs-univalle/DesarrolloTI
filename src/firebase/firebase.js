e7f4dfa830799a27ed82ef8ab63c6771068dda1b
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2025-06-27 11:27:44 -0500

    fix: ruta del css de Formulario.jsx

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAr5NeTVgrpA6acrnSyO508xa_smPlD0vE",
-  authDomain: "ti-desarrollo-pt.firebaseapp.com",
-  projectId: "ti-desarrollo-pt",
-  storageBucket: "ti-desarrollo-pt.firebasestorage.app",
-  messagingSenderId: "349935778130",
-  appId: "1:349935778130:web:866c8b7d012d28b889e28b",
-  measurementId: "G-48FBMC22C7"
-};
+  apiKey: "AIzaSyDVrmLKoIdLTqk0x8nA5QY6-nin0n5wJ3w",
+  authDomain: "proservis-ti.firebaseapp.com",
+  projectId: "proservis-ti",
+  storageBucket: "proservis-ti.firebasestorage.app",
+  messagingSenderId: "138550968182",
+  appId: "1:138550968182:web:724991509be9b175494457",
+  measurementId: "G-XXCF962JSJ"
+};  
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
